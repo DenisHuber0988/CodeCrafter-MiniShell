53c31e804e6e5e161ab2b080a1a86c25670cab6f
denis <dhuber0988@gmail.com>
2024-06-30 00:11:29 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,11 +2,21 @@ import sys
 
 
 # The list of the supported commands.
-SUPPORTED_COMMANDS = [
-    "exit",
-]
+SUPPORTED_COMMANDS = {
+    "exit": "handle_exit",
+    "echo": "handle_echo",
+}
+
 
 ERROR_MESSAGE = "command not found"
+
+
+def handle_exit(_):
+    exit()
+
+
+def handle_echo(*args):
+    print(*args)
 
 
 def main():
@@ -16,14 +26,19 @@ def main():
 
         # Wait for user input
         user_input = input().split()
-        command = user_input.pop(0)
+        try:
+            command = user_input.pop(0)
+        except ValueError:
+            break
+
         args = user_input
 
-        if command not in SUPPORTED_COMMANDS:
+        if command not in SUPPORTED_COMMANDS.keys():
             print(f"{command}: {ERROR_MESSAGE}")
         else:
-            func = getattr(sys, command)
-            func()
+            func_name = SUPPORTED_COMMANDS[command]
+            func = globals()[func_name]
+            func(*args)
 
 
 if __name__ == "__main__":
